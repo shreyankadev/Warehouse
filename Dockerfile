c2287aca9c8df4b3f67eacea7888ea2ed64d8db9
Shreyanka <shreyanka219@gmail.com>
2024-03-25 16:36:53 +0100

    build changes

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,24 @@
-
-FROM openjdk:17
-RUN apt-get update && apt-get install -y maven
+# Stage 1: Build Maven project
+FROM maven:3.8.4-openjdk-17 AS maven_build
 WORKDIR /app
 COPY . /app
 RUN mvn clean package
 
+# Stage 2: MySQL setup
 FROM mysql:latest
 ENV MYSQL_ROOT_PASSWORD=root
-COPY /database/setup.sql /docker-entrypoint-initdb.d/
+COPY --from=maven_build /app/database/setup.sql /docker-entrypoint-initdb.d/
+COPY --from=maven_build /app/target/dropwizard-demo-1.0-SNAPSHOT.jar /app/target/
+COPY --from=maven_build /app/src/main/resources/config.yml /app/src/main/resources/
+COPY . /app
+WORKDIR /app
+
 EXPOSE 8080 3306
 
-# Start MySQL service
+# Start MySQL service and Dropwizard application
 CMD service mysql start && \
     # Initialize MySQL database (
-    mysql -u root -p root -h localhost /app/database/setup.sql && \
-    # Start Dropwizard application
-    java -jar target/dropwizard-demo-1.0-SNAPSHOT.jar server /app/config.yml
+    mysql -u root -p root -h localhost /docker-entrypoint-initdb.d/setup.sql && \
+    # Start Dropwizard application 
+	java -jar /app/target/dropwizard-demo-1.0-SNAPSHOT.jar server /app/src/main/resourcesconfig.yml"
+
--- database/setup.sql
+++ database/setup.sql
@@ -1,3 +1,13 @@
+-- Create the warehouse schema
+CREATE SCHEMA IF NOT EXISTS warehouse;
+
+-- Use the warehouse schema
+USE warehouse;
+
+-- Create tables and insert initial data
+-- (Add your table creation and data insertion statements here)
+
+
 CREATE TABLE Inventory (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL CHECK (name != ''),
@@ -27,3 +37,17 @@ CREATE TABLE inventorylocation (
     FOREIGN KEY (inventoryid) REFERENCES Inventory(id),
     FOREIGN KEY (locationid) REFERENCES Location(locationid)
 );
+
+-- Insert data into the "Inventory" table
+INSERT INTO Inventory (id, name, dimensions, volume, type, status, count)
+VALUES
+    (1, 'Item 1', '10x20x5', 100, 'Type A', 'RECEIVED', 1),
+    (2, 'Item 2', '15x15x15', 337, 'Type B', 'RECEIVED', 1),
+    (3, 'Item 3', '8x8x8', 512, 'Type C', 'RECEIVED', 1);
+
+-- Insert data into the "Location" table
+INSERT INTO Location (location_id, location_name, location_type, dimension, onhand_qty, max_qty)
+VALUES
+    (101, 'Location A', 'Warehouse', '10x20x30', 50, 100),
+    (102, 'Location B', 'Storage', '15x15x15', 100, 200),
+    (103, 'Location C', 'Retail Store', '5x10x5', 20, 50);
